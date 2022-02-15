876794bdbd6ba8fb4e8fcdfae7047963a39721f8
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-02-15 16:43:11 +0100

    Implement RegisterUser component

--- src/store.js
+++ src/store.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    user: null
+  },
+  mutations: {
+    'SET_USER_DATA' (state, payload) {
+      state.user = payload
+      localStorage.setItem('user', JSON.stringify(payload))
+      axios.defaults.headers.common['Authorization'] = `Bearer ${payload.token}`
+    }
+  },
+  actions: {
+    register ({ commit }, payload) {
+      return axios.post('//localhost:3000/register', payload)
+        .then(({ data }) => {
+          commit('SET_USER_DATA', data)
+        })
+    }
+  }
 })
---
+++ src/views/RegisterUser.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <form @submit.prevent="register">
+      <label for="name">Name: </label>
+      <input id="name" v-model="name" name="name" type="text">
+      <label for="email">Email: </label>
+      <input id="email" v-model="email" name="email" type="text">
+      <label for="password">Password: </label>
+      <input id="password" v-model="password" name="password" type="password">
+      <button name="button">
+        Register
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    register () {
+      this.$store.dispatch('register', {
+        name: this.name,
+        email: this.email,
+        password: this.password
+      })
+        .then(() => {
+          this.$router.push({ name: 'dashboard' })
+        })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
